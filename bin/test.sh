3ace2ecca085524e176e55280d4fee38e3afdcc5
hachiyanagi-m <hachiyanagi_m@waku-2.com>
2019-11-11 18:31:06 +0900

    dev

--- bin/test.sh
+++ bin/test.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+VAULT_PASS=`aws ssm get-parameter --name /beluga/ansible/vault-password --with-decryption | jq -r .Parameter.Value`
 echo $1
+cd /tmp && git clone https://github.com/mickey8/sandbox.git
+echo "$VAULT_PASS" | ansible-playbook /tmp/sandobox/ansible/playbooks/test/site.yml --ask-vault-pass
