d00209b771d4528f4bf972e9e1f68ddd30c41496
hachiyanagi-m <hachiyanagi_m@waku-2.com>
2019-11-11 18:36:25 +0900

    dev

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+GIT_TOKEN=`aws ssm get-parameter --name /beluga/git/token --with-decryption | jq -r .Parameter.Value`
 VAULT_PASS=`aws ssm get-parameter --name /beluga/ansible/vault-password --with-decryption | jq -r .Parameter.Value`
 echo $1
-cd /tmp && git clone https://github.com/mickey8/sandbox.git
+cd /tmp && git clone https://mickey8:${GIT_TOKEN}@github.com/mickey8/sandbox.git
 echo "$VAULT_PASS" | ansible-playbook /tmp/sandobox/ansible/playbooks/test/site.yml --ask-vault-pass
