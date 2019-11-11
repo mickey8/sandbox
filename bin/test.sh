1a2d2e9e6536433251b58bacc09bac6fb5069c20
hachiyanagi-m <hachiyanagi_m@waku-2.com>
2019-11-11 19:32:07 +0900

    dev

--- bin/test.sh
+++ bin/test.sh
@@ -4,6 +4,6 @@ VAULT_PASS=`aws ssm get-parameter --region ap-northeast-1 --name /beluga/ansible
 echo $1
 git clone https://mickey8:${GIT_TOKEN}@github.com/mickey8/sandbox.git /tmp/sandbox
 echo "$VAULT_PASS" > ~/.vault_password
-ansible-playbook /tmp/sandbox/ansible/playbooks/test/site.yml --vault-password-file
+ansible-playbook /tmp/sandbox/ansible/playbooks/test/site.yml --vault-password-file ~/.vault_password
 rm -f ~/.vault_password
 rm -rf /tmp/sandbox
